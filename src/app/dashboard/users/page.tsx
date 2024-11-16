411aceaf40cb241abd3e05b04e94c11d17582427
Nishith Pandey <nishith.pandey04@gmail.com>
2024-11-16 22:31:33 +0530

    Dynamic routing
    Signed-off-by: Nishith Pandey <nishith.pandey04@gmail.com>

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,6 +1,24 @@
-'use client';
+import Link from 'next/link';
 
 export default function Users() {
-  console.log('I am a client component');
-  return <h1 className='text-3xl'>Users</h1>;
+  return (
+    <div>
+      <h1>Dashboard Users</h1>
+
+      <ul className='mt-10'>
+        <li>
+          <Link href='/dashboard/users/1'>User 1</Link>
+        </li>
+        <li>
+          <Link href='/dashboard/users/2'>User 2</Link>
+        </li>
+        <li>
+          <Link href='/dashboard/users/3'>User 3</Link>
+        </li>
+        <li>
+          <Link href='/dashboard/users/4'>User 4</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
