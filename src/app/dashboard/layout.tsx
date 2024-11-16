f549ca14d89b400f39552991d14c903f039efff7
Nishith Pandey <nishith.pandey04@gmail.com>
2024-11-16 22:53:17 +0530

    Layout
    Signed-off-by: Nishith Pandey <nishith.pandey04@gmail.com>

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1 className='text-3xl'>DASHBOARD</h1>
+      {children}
+    </div>
+  );
+}
