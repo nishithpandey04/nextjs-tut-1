793df4449f02a6b418a316bf6029530ee36f9c70
Nishith Pandey <nishith.pandey04@gmail.com>
2024-11-16 21:20:54 +0530

    Nested routes
    Signed-off-by: Nishith Pandey <nishith.pandey04@gmail.com>

---
+++ src/app/dashboard/analytics/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export default function Analytics() {
+  console.log('I am a client component');
+  return <h1 className='text-3xl'>Analytics</h1>;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+export default function Dashboard() {
+  console.log('I am a client component');
+  return <h1 className='text-3xl'>Dashboard</h1>;
+}
